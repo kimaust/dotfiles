dce9e419c33d4769c6930a95ab7b7a531c237127
kimaust <kim.aus@outlook.com>
2024-04-14 21:04:03 +0800

    script update

--- install-package/neovim/install.sh
+++ install-package/neovim/install.sh
@@ -1,2 +1,16 @@
-git clone https://github.com/neovim/neovim
+if [ ! -d neovim ]; then
+    git clone https://github.com/neovim/neovim
+    ret_code=$?
+    if [ $ret_code -ne 0 ]; then
+        echo "Failed to clone neovim"
+        return $ret_code
+    fi
+fi
 
+cd neovim
+
+cmake -S cmake.deps -B .deps -G Ninja -D CMAKE_BUILD_TYPE=RelWithDebInfo
+cmake --build .deps
+cmake -B build -G Ninja -D CMAKE_BUILD_TYPE=RelWithDebInfo
+cmake --build build
+    
