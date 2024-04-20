9d4c0d4b73bf2d728cb09fedfedce24e649eaada
kimaust <kim.aus@outlook.com>
2024-04-20 23:07:37 +0800

    comment update

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -5,6 +5,9 @@ return {
         comment.setup()
 
         local api = require("Comment.api")
-        vim.keymap.set("n", "<C-_>", api.toggle.linewise.current, { noremap = true })
+        -- I have no idea why <C-/> works, but <C-_> doesn't.
+        vim.keymap.set({ "n", "i", "v" }, "<C-/>", function()
+            api.toggle.linewise.current()
+        end, { noremap = true, unique = true })
     end,
 }
