06109a1f71089e54631376f9951d70e04e92640b
kimaust <kim.aus@outlook.com>
2024-04-30 22:35:33 +0800

    remove c-t binding

--- nvim/lua/keymaps/keymaps.lua
+++ nvim/lua/keymaps/keymaps.lua
@@ -1,12 +1,17 @@
+local opts = { noremap = true, silent = true }
+
 -- Convenience keymaps.
-vim.keymap.set({ "n", "i", "v" }, "<C-s>", "<Cmd>w<CR>", { noremap = true, silent = true })
-vim.keymap.set({ "i" }, "<C-x>", "<BS>", { noremap = true, silent = true })
-vim.keymap.set({ "n" }, "<C-x>", "xh", { noremap = true, silent = true })
+vim.keymap.set({ "n", "i", "v" }, "<C-s>", "<Cmd>w<CR>", opts)
+vim.keymap.set({ "i" }, "<C-x>", "<BS>", opts)
+vim.keymap.set({ "n" }, "<C-x>", "xh", opts)
 
 -- TODO: Figure out why this isn't working
--- vim.keymap.set({ "i" }, "<C-z>", "<Esc>ua", { noremap = true, silent = true })
-vim.keymap.set({ "n", "v" }, "<C-z>", "u", { noremap = true, silent = true })
-vim.keymap.set({ "n", "v" }, "u", "<Nop>", { noremap = true, silent = true })
+-- vim.keymap.set({ "i" }, "<C-z>", "<Esc>ua",  opts)
+vim.keymap.set({ "n", "v" }, "<C-z>", "u", opts)
+vim.keymap.set({ "n", "v" }, "u", "<Nop>", opts)
+
+vim.keymap.set({ "n", "v", "i", "t" }, "<C-t>", "<Nop>", opts)
+
 -- inoremap <expr> <silent> <cr> pumvisible() ? "<c-y>" : "<c-g>u<cr>"
 -- vim.keymap.set({ "i" }, "<C-r>", function()
 --     vim.cmd("redo")
